e0ebd1fe04ef38aa579592d474ae6232333453cb
lorena olivares <lorena.olivares.perez16@gmail.com>
2023-01-16 19:49:49 -0300

    requisitos 5 e 6

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,33 @@
 import csv
+import json
+import xmltodict
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
     @staticmethod
+    def verificar_arquivo(path):
+        if path.endswith(".csv"):
+            with open(path) as file:
+                return list(csv.DictReader(file))
+        elif path.endswith(".json"):
+            with open(path, "r") as file:
+                return json.load(file)
+        else:
+            with open(path) as file:
+                return xmltodict.parse(file.read())["dataset"]["record"]
+        # https://algoritmosempython.com.br/cursos/programacao-python/strings/
+        # A string s termina com "mundo"?
+        # print(s.endswith("mundo"))
+
+    @staticmethod
     def import_data(path, tipo):
 
-        with open(path) as file:
-            relatorio_csv = list(csv.DictReader(file))
+        arquivo = Inventory.verificar_arquivo(path)
         if tipo == "simples":
-            return SimpleReport.generate(relatorio_csv)
+            return SimpleReport.generate(arquivo)
         elif tipo == "completo":
-            return CompleteReport.generate(relatorio_csv)
+            return CompleteReport.generate(arquivo)
         else:
             raise ValueError("Tipo inválido")
